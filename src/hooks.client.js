a2c1bd5a4e523338bec1f6542d482cdf6d5c95cb
Tee Ming <chewteeming01@gmail.com>
2023-04-25 03:00:44 +0800

    test

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -1,3 +1,5 @@
-export function handleError(event) {
-  console.error(event.error);
+export async function handleError({ error, event }) {
+  const errorId = crypto.randomUUID();
+  // example integration with https://sentry.io/
+  console.log(errorId, error);
 }
