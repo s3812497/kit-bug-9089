fd928e5a182f3d01edf2d7880b1f138502e66006
Tee Ming <chewteeming01@gmail.com>
2023-04-25 02:37:39 +0800

    version

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		version: {
-			name: 'same-version'
+			name: child_process.execSync('git rev-parse HEAD').toString().trim()
 		}
 	}
 };
